dac4fbde029a59ef66d784eb5c7a506db89ed9d6
LeviticusRS <keylog0728@gmail.com>
2021-10-30 01:49:36 -0400

    Implement fromJson and toJson for encoding and decoding the user

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -8,8 +8,21 @@ class User {
   User(this.firstName, this.lastName, this.phoneNumber, this.email,
       this.biography);
 
+  User.fromJson(Map<String, dynamic> json): firstName = json['firstName'],
+        lastName = json['lastName'], phoneNumber = json['phoneNumber'],
+        email = json['email'], biography = json['biography'];
+
   static User fetchDefault() {
     return User("Joshua", "Ransom", "910-105-1039", "joshua.ransom@approachablegeek.com", "Potential Future Approachable Geek, Looking to find a new home to build and experience new things.");
   }
+
+  Map toJson() => {
+    'firstName': firstName,
+    'lastName': lastName,
+    'phoneNumber': phoneNumber,
+    'email': email,
+    'biography': biography,
+  };
+
 }
 
