abb8ffaf0945a201de00f7eec027b1594a7c539c
LeviticusRS <keylog0728@gmail.com>
2021-10-30 01:49:44 -0400

    Finished up the project

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
@@ -5,6 +6,9 @@ import 'dart:io' show File, Platform;
 
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:project_geek/constants.dart';
+import 'package:project_geek/models/type.dart';
+import 'package:project_geek/models/user.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key, required this.title}) : super(key: key);
@@ -21,6 +25,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   void initState() {
     super.initState();
+    _loadProfileJson();
   }
 
   @override
@@ -38,7 +43,6 @@ class _ProfileScreenState extends State<ProfileScreen> {
               Column(
                 children: <Widget>[
                   Container(
-                    height: 250.0,
                     color: Colors.white,
                     child: Column(
                       children: <Widget>[
@@ -49,7 +53,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                               crossAxisAlignment: CrossAxisAlignment.center,
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: <Widget>[
-                                FutureBuilder(future: _profileFile,
+                                FutureBuilder(future: _profileImageFile,
                                     builder: (BuildContext context, AsyncSnapshot<File> snapshot) {
                                       return snapshot.data != null ? Container(
                                           width: 140.0,
@@ -92,7 +96,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                                       radius: 25.0,
                                       child: IconButton(
                                         icon: const Icon(Icons.edit, color: Colors.white, size: 22.0),
-                                        tooltip: 'Go Back',
+                                        tooltip: 'Edit Photo',
                                         onPressed: () {
                                           setState(() {
                                             if (Platform.isAndroid) {
@@ -105,7 +109,12 @@ class _ProfileScreenState extends State<ProfileScreen> {
                                   ],
                                 )),
                           ]),
-                        )
+                        ),
+                        buildFirstName(),
+                        buildLastName(),
+                        buildEmail(),
+                        buildPhoneNumber(),
+                        buildBiography(),
                       ],
                     ),
                   ),
@@ -123,7 +132,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
       maxWidth: 1800,
       maxHeight: 1800,
     );
-    final file = await _profileFile;
+    final file = await _profileImageFile;
     setState(() {
       if (image != null) {
         imageFile = Image.file(File(image.path)).image;
@@ -132,13 +141,326 @@ class _ProfileScreenState extends State<ProfileScreen> {
     });
   }
 
+  _writeProfileJson() async {
+    final file = await _profileInfoFile;
+    setState(() {
+      file.writeAsString(jsonEncode(user));
+    });
+  }
+
+  _loadProfileJson() async {
+    final file = await _profileInfoFile;
+    if (file.existsSync()) {
+      setState(() {
+        Map<String, dynamic> userMap = jsonDecode(file.readAsStringSync());
+        user = User.fromJson(userMap);
+      });
+    }
+  }
+
   Future<String> get _localPath async {
     final directory = await getApplicationDocumentsDirectory();
     return directory.path;
   }
 
-  Future<File> get _profileFile async {
+  Future<File> get _profileImageFile async {
     final path = await _localPath;
     return File('$path/profile.jpg');
+  }
+
+  Future<File> get _profileInfoFile async {
+    final path = await _localPath;
+    return File('$path/profile.json');
+  }
+
+  Widget buildBiography() => Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Biography',
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w500,
+              color: Colors.grey,
+            ),
+          ),
+          const SizedBox(height: 1),
+          Container(
+              decoration: const BoxDecoration(
+                  border: Border(
+                      bottom: BorderSide(
+                        color: Colors.grey,
+                        width: 1,
+                      ))),
+              child: Row(children: [
+                Expanded(
+                    child: TextButton(
+                        onPressed: () {
+                          _inputDialogue(context, Type.biography);
+                        },
+                        child: Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 10, 10, 10),
+                            child: Align(
+                                alignment: Alignment.topLeft,
+                                child: Text(
+                                  user.biography,
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    height: 1.4,
+                                  ),
+                                ))))),
+                const Icon(
+                  Icons.keyboard_arrow_right,
+                  color: Colors.grey,
+                  size: 40.0,
+                )
+              ]))
+        ],
+      ));
+  Widget buildFirstName() => Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'First Name',
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w500,
+              color: Colors.grey,
+            ),
+          ),
+          const SizedBox(height: 1),
+          Container(
+              decoration: const BoxDecoration(
+                  border: Border(
+                      bottom: BorderSide(
+                        color: Colors.grey,
+                        width: 1,
+                      ))),
+              child: Row(children: [
+                Expanded(
+                    child: TextButton(
+                        onPressed: () {
+                          _inputDialogue(context, Type.firstName);
+                        },
+                        child: Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 10, 10, 10),
+                            child: Align(
+                                alignment: Alignment.topLeft,
+                                child: Text(
+                                  user.firstName,
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    height: 1.4,
+                                  ),
+                                ))))),
+                const Icon(
+                  Icons.keyboard_arrow_right,
+                  color: Colors.grey,
+                  size: 40.0,
+                )
+              ]))
+        ],
+      ));
+  Widget buildLastName() => Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Last Name',
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w500,
+              color: Colors.grey,
+            ),
+          ),
+          const SizedBox(height: 1),
+          Container(
+              decoration: const BoxDecoration(
+                  border: Border(
+                      bottom: BorderSide(
+                        color: Colors.grey,
+                        width: 1,
+                      ))),
+              child: Row(children: [
+                Expanded(
+                    child: TextButton(
+                        onPressed: () {
+                          _inputDialogue(context, Type.lastName);
+                        },
+                        child: Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 10, 10, 10),
+                            child: Align(
+                                alignment: Alignment.topLeft,
+                                child: Text(
+                                  user.lastName,
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    height: 1.4,
+                                  ),
+                                ))))),
+                const Icon(
+                  Icons.keyboard_arrow_right,
+                  color: Colors.grey,
+                  size: 40.0,
+                )
+              ]))
+        ],
+      ));
+  Widget buildPhoneNumber() => Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Phone Number',
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w500,
+              color: Colors.grey,
+            ),
+          ),
+          const SizedBox(height: 1),
+          Container(
+              decoration: const BoxDecoration(
+                  border: Border(
+                      bottom: BorderSide(
+                        color: Colors.grey,
+                        width: 1,
+                      ))),
+              child: Row(children: [
+                Expanded(
+                    child: TextButton(
+                        onPressed: () {
+                          _inputDialogue(context, Type.phoneNumber);
+                        },
+                        child: Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 10, 10, 10),
+                            child: Align(
+                                alignment: Alignment.topLeft,
+                                child: Text(
+                                  user.phoneNumber,
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    height: 1.4,
+                                  ),
+                                ))))),
+                const Icon(
+                  Icons.keyboard_arrow_right,
+                  color: Colors.grey,
+                  size: 40.0,
+                )
+              ]))
+        ],
+      ));
+  Widget buildEmail() => Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Email',
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w500,
+              color: Colors.grey,
+            ),
+          ),
+          const SizedBox(height: 1),
+          Container(
+              decoration: const BoxDecoration(
+                  border: Border(
+                      bottom: BorderSide(
+                        color: Colors.grey,
+                        width: 1,
+                      ))),
+              child: Row(children: [
+                Expanded(
+                    child: TextButton(
+                        onPressed: () {
+                          _inputDialogue(context, Type.email);
+                        },
+                        child: Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 10, 10, 10),
+                            child: Align(
+                                alignment: Alignment.topLeft,
+                                child: Text(
+                                  user.email,
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    height: 1.4,
+                                  ),
+                                ))))),
+                const Icon(
+                  Icons.keyboard_arrow_right,
+                  color: Colors.grey,
+                  size: 40.0,
+                )
+              ]))
+        ],
+      ));
+
+  final TextEditingController _textFieldController = TextEditingController();
+
+  Future<void> _inputDialogue(BuildContext context, Type type) async {
+    _textFieldController.clear();
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Edit ${type.displayTitle}'),
+          content: TextField(
+            controller: _textFieldController,
+            decoration: InputDecoration(hintText: "${type.displayTitle}"),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                String value = _textFieldController.value.text;
+                switch (type) {
+                  case Type.firstName:
+                    if (value.isValidName) {
+                      user.firstName = value;
+                    }
+                    break;
+                  case Type.lastName:
+                    if (value.isValidName) {
+                      user.lastName = value;
+                    }
+                    break;
+                  case Type.email:
+                    if (value.isValidEmail) {
+                      user.email = value;
+                    }
+                    break;
+                  case Type.phoneNumber:
+                    if (value.isValidPhone) {
+                      user.phoneNumber = value;
+                    }
+                    break;
+                  case Type.biography:
+                    user.biography = _textFieldController.value.text;
+                    break;
+                }
+
+                _writeProfileJson();
+                Navigator.pop(context);
+              },
+            ),
+            TextButton(
+              child: const Text('CANCEL'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            )
+          ],
+        );
+      },
+    );
   }
 }
