fcc6d963ee932600996bd5bccefc84a136c3fe36
LeviticusRS <keylog0728@gmail.com>
2021-10-30 01:38:39 -0400

    Global user, and regex validation

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,24 @@
+import 'models/user.dart';
+
 const homeScreen = '/';
 const editScreen = '/Edit';
+var user = User.fetchDefault();
 
+extension ValidateString on String {
+  bool get isValidEmail {
+    final emailRegExp = RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    return emailRegExp.hasMatch(this);
+  }
+
+  bool get isValidName {
+    final nameRegExp = RegExp(r"^\s*([A-Za-z]{1,}([\.,] |[-']| ))+[A-Za-z]+\.?\s*$");
+    return nameRegExp.hasMatch(this);
+  }
+
+  bool get isValidPhone {
+    final phoneRegExp = RegExp(r"^\+?0[0-9]{10}$");
+    return phoneRegExp.hasMatch(this);
+  }
+
+
+}
